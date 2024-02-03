d8d765102050c0d0b898b0a1b86750e722380ac5
lmbrumleve <142369731+lmbrumleve@users.noreply.github.com>
2024-02-02 18:02:40 -0600

    task 2 progress

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,27 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank
+    @Size(min=3, max=50, message="Value must be between 3 and 50 characters long.")
+    private String location;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public Employer() {}
 }
